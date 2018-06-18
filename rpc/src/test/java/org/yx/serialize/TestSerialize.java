ac1d5009654d2db019df1cace2eb55f56b953b6d
mxzenf <93839187@qq.com>
2018-06-18 09:46:36 +0800

    增加buffer测试

--- rpc/src/test/java/org/yx/serialize/TestSerialize.java
+++ rpc/src/test/java/org/yx/serialize/TestSerialize.java
@@ -1,8 +1,11 @@
 package org.yx.serialize;
 
+import junit.framework.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.yx.bean.RpcRequest;
+
+import java.nio.ByteBuffer;
 
 /**
  * Created by 杨欣 on 2018/6/10.
@@ -19,5 +22,16 @@ public class TestSerialize {
         rr.setId("1");
         byte[] bytes = so.serialize(rr);
         System.out.print(so.deserialize(bytes));
+    }
+    @Test
+    public void test_buff(){
+        RpcRequest rr = new RpcRequest();
+        rr.setId("1");
+        ByteBuffer bb = ByteBuffer.allocate(2048);
+        bb.clear();
+        byte[] bytes = so.serialize(rr);
+        bb.put(bytes);
+        bb.flip();
+        Assert.assertEquals("1", ((RpcRequest)so.deserialize(bb.array())).getId());
     }
 }
