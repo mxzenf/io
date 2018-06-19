3cd225dd5e35574637c269a13fffa4af7e3f0ae5
mxzenf <93839187@qq.com>
2018-06-19 23:16:19 +0800

    增加probuff序列化

---
+++ rpc/src/main/java/org/yx/serialize/ProtostuffSerialize.java
@@ -0,0 +1,54 @@
+package org.yx.serialize;
+
+import com.dyuproject.protostuff.LinkedBuffer;
+import com.dyuproject.protostuff.ProtostuffIOUtil;
+import com.dyuproject.protostuff.runtime.RuntimeSchema;
+import com.dyuproject.protostuff.Schema;
+import org.objenesis.Objenesis;
+import org.objenesis.ObjenesisStd;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * Created by 杨欣 on 2018/6/19.
+ */
+public class ProtostuffSerialize implements SerializeObject {
+    private static Map<Class<?>, Schema<?>> cachedSchema
+            = new ConcurrentHashMap<>();
+    private static Objenesis objenesis = new ObjenesisStd(true);
+    @Override
+    public <T> byte[] serialize(T o) {
+        Class<T> cls = (Class<T>)o.getClass();
+        LinkedBuffer buffer = LinkedBuffer.allocate(LinkedBuffer.DEFAULT_BUFFER_SIZE);
+        try {
+            Schema<T> schema = getSchema(cls);
+            return ProtostuffIOUtil.toByteArray(o, schema, buffer);
+        } catch (Exception e) {
+            throw new IllegalStateException(e.getMessage(), e);
+        } finally {
+            buffer.clear();
+        }
+    }
+
+    @Override
+    public <T> T deserialize(byte[] bytes, Class<T> cls) {
+        try {
+            T message = objenesis.newInstance(cls);
+            Schema<T> schema = getSchema(cls);
+            ProtostuffIOUtil.mergeFrom(bytes, message, schema);
+            return message;
+        } catch (Exception e) {
+            throw new IllegalStateException(e.getMessage(), e);
+        }
+    }
+    @SuppressWarnings("unchecked")
+    private static <T> Schema<T> getSchema(Class<T> cls) {
+        Schema<T> schema = (Schema<T>) cachedSchema.get(cls);
+        if (schema == null) {
+            schema = RuntimeSchema.createFrom(cls);
+            cachedSchema.put(cls, schema);
+        }
+        return schema;
+    }
+}
