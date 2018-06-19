f364270cb2f318378a9e26652a800a12d0d54999
mxzenf <93839187@qq.com>
2018-06-20 00:03:15 +0800

    按照protobuff序列化修改接口

--- rpc/src/main/java/org/yx/serialize/SerializeObject.java
+++ rpc/src/main/java/org/yx/serialize/SerializeObject.java
@@ -2,8 +2,15 @@ package org.yx.serialize;
 
 /**
  * Created by 杨欣 on 2018/6/10.
+ * 按照其他rpc框架对象解码优化接口
+ *java自带的serializable本地化持久对象没问题，
+ *如果通过nio实现就是巨坑，参考netty的java原始
+ *序列化实现，netty实现了自己的objectoutputstream
+ *和objectinputstream，这部分google说明比较少。
+ *大多数都认为使用io socket就行了。综合考虑后，
+ *决定采取protobuff实现对象传输序列化
  */
 public interface SerializeObject {
-    byte[] serialize(Object o);
-    Object deserialize(byte[] bytes);
+    <T> byte[] serialize(T o);
+    <T> T deserialize(byte[] bytes, Class<T> cls);
 }
