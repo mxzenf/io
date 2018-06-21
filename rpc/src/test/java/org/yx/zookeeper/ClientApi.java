da7fde652df0d94b492e9cf51f705aa9449488f8
mxzenf <93839187@qq.com>
2018-06-21 22:21:54 +0800

    添加创建节点,设置数据,删除节点测试用例

--- rpc/src/test/java/org/yx/zookeeper/ClientApi.java
+++ rpc/src/test/java/org/yx/zookeeper/ClientApi.java
@@ -13,6 +13,8 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.util.List;
+
 import org.apache.zookeeper.ZooDefs.Ids;
 
 /**
@@ -20,6 +22,7 @@ import org.apache.zookeeper.ZooDefs.Ids;
  */
 public class ClientApi implements AsyncCallback.StringCallback {
     private ZooKeeper zk;
+    private String path = "/zookeeper";
     LoggerFactory logger;
     @Before
     public void init(){
@@ -36,14 +39,47 @@ public class ClientApi implements AsyncCallback.StringCallback {
     @Test
     public void test_create() throws Exception {
         String add = "127.0.0.1:9999";
-        String path = "/zookeeper";
         zk.create(path, add.getBytes(Charset.forName("utf8")), Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL, this,null);
     }
 
     @Test
     public void test_exist_path() throws KeeperException, InterruptedException {
-        Stat stat = zk.exists("/zookeeper", false);
+        Stat stat = zk.exists(path, false);
         Assert.assertNotNull(stat);
+    }
+
+    @Test
+    public void test_setData() throws KeeperException, InterruptedException {
+        String data = "杨欣";
+        Stat stat = zk.setData(path,data.getBytes(Charset.forName("utf8")),-1);
+        Assert.assertNotNull(stat);
+    }
+
+    @Test
+    public void test_getData() throws KeeperException, InterruptedException {
+        byte[] data = zk.getData(path, true, null);
+        Assert.assertEquals("杨欣", new String(data));
+    }
+
+    @Test
+    public void test_createChild(){
+        zk.create(path+"/node1", "127.0.0.1:9999".getBytes(Charset.forName("utf8")), Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL, this,null);
+        zk.create(path+"/node2", "127.0.0.1:9998".getBytes(Charset.forName("utf8")), Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL, this,null);
+
+    }
+
+    @Test
+    public void test_getChildred() throws KeeperException, InterruptedException {
+        List<String> children = zk.getChildren(path, null);
+        Assert.assertEquals(2, children.size());
+    }
+
+    @Test
+    public void test_delNode() throws KeeperException, InterruptedException {
+        zk.create(path+"/node3", "127.0.0.1:9998".getBytes(Charset.forName("utf8")), Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL, this,null);
+        Assert.assertEquals(3, zk.getChildren(path, null).size());
+        zk.delete(path+"/node3", -1);
+        Assert.assertEquals(2, zk.getChildren(path, null).size());
     }
 
     @After
