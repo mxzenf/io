5b536a4f3bec5e19b0e95a4bab71dff2d23e37a5
mxzenf <93839187@qq.com>
2018-06-23 09:03:55 +0800

    增加服务发现注册接口

---
+++ rpc/src/main/java/org/yx/service/ServiceDiscovery.java
@@ -0,0 +1,8 @@
+package org.yx.service;
+
+/**
+ * Created by 杨欣 on 2018/6/23.
+ */
+public interface ServiceDiscovery {
+    String discovery(String serviceName);
+}
---
+++ rpc/src/main/java/org/yx/service/ServiceRegistry.java
@@ -0,0 +1,8 @@
+package org.yx.service;
+
+/**
+ * Created by 杨欣 on 2018/6/23.
+ */
+public interface ServiceRegistry {
+    void registry(String serviceName, String serviceAddress);
+}
