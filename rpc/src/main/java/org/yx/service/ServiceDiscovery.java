eb323836a186532d8ad338bf9af25314a70d01dd
mxzenf <93839187@qq.com>
2018-06-23 09:35:44 +0800

    添加类说明

--- rpc/src/main/java/org/yx/service/ServiceDiscovery.java
+++ rpc/src/main/java/org/yx/service/ServiceDiscovery.java
@@ -2,6 +2,8 @@ package org.yx.service;
 
 /**
  * Created by 杨欣 on 2018/6/23.
+ * 根据服务名发现在zookeeper上面注册的服务
+ *
  */
 public interface ServiceDiscovery {
     String discovery(String serviceName);
--- rpc/src/main/java/org/yx/service/ServiceRegistry.java
+++ rpc/src/main/java/org/yx/service/ServiceRegistry.java
@@ -2,6 +2,7 @@ package org.yx.service;
 
 /**
  * Created by 杨欣 on 2018/6/23.
+ * 将服务信息注册要zookeeper上
  */
 public interface ServiceRegistry {
     void registry(String serviceName, String serviceAddress);
